94c28cc4f23c8aae93a498a5cc41832d88035dea
Alex Hageman <alexhageman4@gmail.com>
2026-04-06 17:11:22 -0500

    bootstrap

--- app.py
+++ app.py
@@ -3,12 +3,19 @@
 # Alex Hageman 4/3/2026
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
